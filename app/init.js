20afc0b79824aa7974373553572e924ef75d6dc0
Nathan Elg <elgn@students.wwwu.edu>
2017-05-05 12:43:10 -0600

    first commit

---
+++ app/init.js
@@ -0,0 +1,9 @@
+var app ={
+  controllers: {
+    weatherController: new weatherController,
+    imageController: new imageController,
+    quoteController: new QuoteController,
+    todoController: new TodoController
+
+  }
+}
